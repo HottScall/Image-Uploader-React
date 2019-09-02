eda31d2cf4aac9930cd98e61a5f695c978c3ccb7
Scott <Scotthall1983@hotmail.com>
2019-09-02 13:17:03 +0100

    Created a Spinner.js which will act as the spinner whilst the image content is uploading.

---
+++ src/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawseome/react-fontawesome';
+import { faBowlingBall } from '@forawseome/free-solid-svg-icons';
+
+export default () =>
+  <div className='spinner fadein'>
+    <FontAwesomeIcon icon{faBowlingBall} size='5x' color='#3B5998' />
+  <div/>
