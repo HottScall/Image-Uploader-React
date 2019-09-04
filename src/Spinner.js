3f079da8fa0bf0aff6d92b7f23e7ad4c2f2892c6
Scott <Scotthall1983@hotmail.com>
2019-09-04 10:19:15 +0100

    Minor code clean up of sytax errors.

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawseome/react-fontawesome';
-import { faBowlingBall } from '@forawseome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawseome/react-fontawesome";
+import { faBowlingBall } from "@forawseome/free-solid-svg-icons";
 
-export default () =>
-  <div className='spinner fadein'>
-    <FontAwesomeIcon icon{faBowlingBall} size='5x' color='#3B5998' />
-  <div/>
+export default () => (
+  <div className="spinner fadein">
+    <FontAwesomeIcon icon={faBowlingBall} size="5x" color="#3B5998" />
+  </div>
+);
