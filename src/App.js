33be5b3b4c05335049a73b930a489d7158828521
Scott <Scotthall1983@hotmail.com>
2019-09-04 10:32:20 +0100

    Updated the onChange function with a fetch request

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ import Button from "./Button";
 import { API_URL } from "./config";
 import "./App.css";
 
-
-export default class App extends Component{
-
+export default class App extends Component {
   state = {
     uploading: false,
     images: []
-  }
+  };
 
-  onChange = e =>{
-    const files = Array.from(e.target.files)
-    this.setState( {uploading: true} )
+  onChange = e => {
+    const files = Array.from(e.target.files);
+    this.setState({ uploading: true });
 
-    const formData = new formData()
+    const formData = new formData();
 
-    files.forEach((file, i )=>{
-      formData.append(i, file)
+    files.forEach((file, i) => {
+      formData.append(i, file);
+    });
+
+    fetch(`${API_URL}/image-upload`, {
+      method: "POST",
+      body: formData
     })
-  }
-
+      .then(res => res.json())
+      .then(images => {
+        this.setState({
+          uploading: false,
+          images
+        });
+      });
+  };
 }
