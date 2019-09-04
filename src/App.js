bdda092b7fbb26785e7d8159b40e7d7d8157816b
Scott <Scotthall1983@hotmail.com>
2019-09-04 10:27:11 +0100

    Created an App.js file, imported relevant files. Set state properties of uploading to false and images to an empty array. Created an onChange event function which sets state to true and iterates through the new upload.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Spinner from "./Spinner";
+import Image from "./Image";
+import Button from "./Button";
+import { API_URL } from "./config";
+import "./App.css";
+
+
+export default class App extends Component{
+
+  state = {
+    uploading: false,
+    images: []
+  }
+
+  onChange = e =>{
+    const files = Array.from(e.target.files)
+    this.setState( {uploading: true} )
+
+    const formData = new formData()
+
+    files.forEach((file, i )=>{
+      formData.append(i, file)
+    })
+  }
+
+}
